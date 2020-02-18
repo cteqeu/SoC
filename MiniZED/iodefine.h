dd5c4ac6c8a4da399ce1affafbb3dcb07d4ca117
Vincent Claes <pxltech@users.noreply.github.com>
2020-02-18 12:42:20 +0100

    Create iodefine.h
    iodefine header for minized; thanks to Randy Thiemann

---
+++ MiniZED/iodefine.h
@@ -0,0 +1,75 @@
+#ifndef IODEFINE_H__
+#define IODEFINE_H__
+
+#define B7 12
+#define EMMC_CLK_R 12
+#define B10 11
+#define EMMC_CMD 11
+#define D6 10
+#define EMMC_D0 10
+#define C6 13
+#define EMMC_D1 13
+#define B9 14
+#define EMMC_D2 14
+#define D10 15
+#define EMMC_D3 15
+
+#define D13 49
+#define FPGA_RXD_IN 49
+#define B12 48
+#define FPGA_TXD_OUT 48
+
+#define B6 8
+#define MIO8 8
+
+#define B5 9
+#define PS_2_AS_RST 9
+
+#define C13 53
+#define PS_LED_G 53
+#define A12 52
+#define PS_LED_R 52
+#define D8 0
+#define PS_PB 0
+
+#define A10 6
+#define QSPI_CLK 6
+#define A5 1
+#define QSPI_CS_N 1
+#define A8 2
+#define QSPI_D0 2
+#define A7 3
+#define QSPI_D1 3
+#define C8 4
+#define QSPI_D2 4
+#define A9 5
+#define QSPI_D3 5
+
+#define B14 36
+#define USB_CLK 36
+#define E15 32
+#define USB_D0 32
+#define C11 33
+#define USB_D1 33
+#define D15 34
+#define USB_D2 34
+#define A14 35
+#define USB_D3 35
+#define A15 28
+#define USB_D4 28
+#define C14 37
+#define USB_D5 37
+#define A13 38
+#define USB_D6 38
+#define D14 39
+#define USB_D7 39
+#define D11 29
+#define USB_DIR 29
+#define C12 31
+#define USB_NXT 31
+#define D9 7
+#define USB_RST_N 7
+#define B15 30
+#define USB_STP 30
+
+#endif
