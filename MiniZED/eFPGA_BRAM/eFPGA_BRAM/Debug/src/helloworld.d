a62099c06c9aa9c258b4d9058c87761bc542ea9b
Vincent Claes <pxltech@users.noreply.github.com>
2020-03-24 09:22:07 +0100

    BRAM Example

---
+++ MiniZED/eFPGA_BRAM/eFPGA_BRAM/Debug/src/helloworld.d
@@ -0,0 +1,21 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_types.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters_ps.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/bspconfig.h:
---
+++ MiniZED/eFPGA_BRAM/eFPGA_BRAM/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters_ps.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_cache.h \
+ C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_types.h \
+ ../src/platform_config.h
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xparameters_ps.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_cache.h:
+
+C:/devWorks/FPGA/eFPGA_BRAM/eFPGA_BRAM_1/export/eFPGA_BRAM_1/sw/eFPGA_BRAM_1/standalone_domain/bspinclude/include/xil_types.h:
+
+../src/platform_config.h:
